f7d8e793d2b332077f4cdadce75bb34cf6fd762a
Augustin Kim <austinkim14@gmail.com>
2021-11-29 15:17:53 -0800

    initial

---
+++ Fundamentals/Fundamentals/For_Loop_Basic_1.py
@@ -0,0 +1,21 @@
+for x in range(1,151):
+    print(x)
+for y in range(5,1005, 5):
+    print(y)
+for z in range(1,101):
+    if z % 10 == 0:
+        print("Coding Dojo")
+    elif z % 5 == 0:
+        print("Coding")
+    else:
+        print(z)
+sum = 0
+for v in range(1,5000000):
+    if v % 2 !=0:
+        sum = sum + v
+print(sum)
+for c in range(2018,0,-4):
+    print(c)
+for a in range(2,10):
+    if a % 3 == 0:
+        print(a)
